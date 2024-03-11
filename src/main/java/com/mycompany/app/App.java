4ecde0fb7891590ec8b396cd43151f89aba58c3f
g-enli <gokcenenli@gmail.com>
2024-03-11 13:46:00 +0300

    input4 not entered case fixed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -66,7 +66,8 @@ public class App
           System.out.println(gemList);
           //input4 için
           String input4 = req.queryParams("input4").replaceAll("\\s","");
-          if (input4!="" || input4!=null)//or will get 500 internal error
+          if (input4=="")
+          input4="0";
           int gemValue = Integer.parseInt(input4);
           System.out.println(gemValue);
           
