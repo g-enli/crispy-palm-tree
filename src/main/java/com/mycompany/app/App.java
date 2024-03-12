5b767518e8be3ab481167742129bfc105cd8daaf
g-enli <gokcenenli@gmail.com>
2024-03-12 10:27:34 +0300

    check null first and keep the input order

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.AbstractMap;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
